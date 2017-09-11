c80a0f8bde7b9f5c39030f6dcd851f65f82e65c1
v1ym <jpbarker2@gmail.com>
2017-09-11 08:36:23 -0400

    zip_script

---
+++ zip_script.ps1
@@ -0,0 +1,18 @@
+#removes current zip and zips contents in one version higher
+#for uploading .php files to AWS
+
+#change path to whichever folder you're working on
+$path = ".\soc"
+$destinationpath = ".\soc"
+
+
+#for removing and increasing versions
+$i = [string[]](Get-ChildItem $path *.zip) | Out-String
+$length = $i.length - 11
+$version = $i.Substring(5, $length)
+
+$version = [int]$version + 1
+write-host "Version: $version"
+
+rm $path/*.zip
+compress-archive -Path "$path\*" -DestinationPath "$destinationpath\soc v$version.zip"
